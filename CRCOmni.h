d91773cb0a026de3f346967df86429897be25766
Зонов Валерий / Zonov Valerij <corvinalive@yandex.ru>
2012-02-28 23:41:00 +0300

    Fisrt commit. Ready to use

---
+++ CRCOmni.h
@@ -0,0 +1,7 @@
+//---------------------------------------------------------------------------
+
+#ifndef CRCOmniH
+#define CRCOmniH
+//---------------------------------------------------------------------------
+unsigned long CRCOmni (unsigned char* buf,unsigned long length);
+#endif
